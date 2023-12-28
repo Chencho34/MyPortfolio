7876ff228690f8bdc7745a415842f34bd1434b4d
Armando <armandocrescencio343@gmail.com>
2023-12-28 12:38:48 -0600

    set vite config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://Chencho34.github.io/MyPortfolio',
   server: {
     host: '192.168.1.69'
   }
