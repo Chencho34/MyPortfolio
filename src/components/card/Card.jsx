5d0bd408632cfff16379ecb59f9ee951de840eae
Armando <armandocrescencio343@gmail.com>
2024-01-04 12:10:54 -0600

    ADD: functional buttom git

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,7 +1,7 @@
 import { FaGithub } from 'react-icons/fa'
 import { MdNavigateNext } from 'react-icons/md'
 
-export default function Card({ title, image, description, tecnologies }) {
+export default function Card({ title, image, description, tecnologies, github }) {
   return (
     <section className='w-[340px] bg-slate-50 dark:bg-[#191919] hover:shadow-lg shadow-md dark:hover:shadow-neutral-950  rounded-md transition-all duration-500 overflow-hidden '>
       <figure className='h-48 rounded-b-xl overflow-hidden'>
@@ -25,9 +25,9 @@ export default function Card({ title, image, description, tecnologies }) {
           <button className='flex items-center justify-evenly bg-[#242424] dark:bg-gray-300 dark:text-gray-950  py-1 rounded-md text-white hover:text-gray-300 dark:hover:text-gray-800 font-semibold text-sm w-20 duration-500 transition-colors'>
             Demo <MdNavigateNext />
           </button>
-          <button className='flex items-center justify-evenly border border-[#242424] dark:border-gray-300  dark:text-gray-300  py-1 rounded-md text-[#242424] hover:text-gray-300 dark:hover:text-gray-700 font-semibold text-sm w-20 duration-500 transition-colors'>
+          <a href={github} target='_blank' className='flex items-center justify-evenly border border-[#242424] dark:border-gray-300  dark:text-gray-300  py-1 rounded-md text-[#242424] hover:text-gray-300 dark:hover:text-gray-700 font-semibold text-sm w-20 duration-500 transition-colors' rel='noreferrer'>
             Git <FaGithub />
-          </button>
+          </a>
         </article>
       </section>
     </section>
--- src/containers/cards/Cards.jsx
+++ src/containers/cards/Cards.jsx
@@ -4,8 +4,8 @@ import { projects } from '../../constants'
 export default function Cards () {
   return (
     <>
-      {projects.map(({ id, title, image, description, tecnologies }) => (
-          <Card key={id} image={image} title={title} description={description} tecnologies={tecnologies} /> 
+      {projects.map(({ id, title, image, description, tecnologies, github }) => (
+          <Card key={id} image={image} title={title} description={description} tecnologies={tecnologies} github={github} /> 
       ))}
     </>
   )
