d34783e3c0dd3c5030912d8a64ad1587293482a5
Armando <armandocrescencio343@gmail.com>
2024-01-06 13:42:02 -0600

    SET: Fade animation in home, about y projects pages

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,3 +1,4 @@
+import { Fade } from 'react-awesome-reveal'
 import { Container, Wrapper } from '../../components'
 import { skills } from '../../constants'
 
@@ -6,29 +7,39 @@ export default function About() {
     <Wrapper>
       <Container>
         <section className='flex flex-col justify-center w-full gap-8'>
-          <h1 className='text-6xl/tight font-bold text-[#242424] dark:text-gray-300  mb-10'>welcome</h1>
-          <section className='flex flex-col gap-20'>
+          <Fade direction='left' duration={300} big>
+            <h1 className='text-6xl/tight font-bold text-[#242424] dark:text-gray-300  mb-10'>Welcome</h1>
+          </Fade>
+          <section className='flex flex-col bg-cyan-600 gap-20'>
             <article className='lg:flex gap-9 items-start justify-between'>
-              <article className='w-40'>
-                <div className='w-20 h-[3px] bg-gradient-to-r from-blue-500 to-cyan-400 rounded-lg' />
-                <h1 className='text-4xl font-bold text-[#242424] dark:text-gray-300 tracking-widest pb-4'>About</h1>
-              </article>
-              <p className='text-sm font-medium text-primary text-balance lg:w-[500px]'>Hola! Soy Armando, un universitario de 21 años apasionado del desarrollo web autodidacta. Mi fascinación por la creación de sitios web comenzó como un hobby y ha evolucionado en una dedicación constante. El desarrollo web me ha permitido adquirir un amplio conocimiento en diferentes áreas, desde el diseño frontend hasta la implementación backend.</p>
+              <Fade direction='left' duration={400}>
+                <article className='w-40'>
+                  <div className='w-20 h-[3px] bg-gradient-to-r from-blue-500 to-cyan-400 rounded-lg' />
+                  <h1 className='text-4xl font-bold text-[#242424] dark:text-gray-300 tracking-widest pb-4'>About</h1>
+                </article>
+              </Fade>
+              <Fade duration={400} direction='up'>
+                <p className='text-sm font-medium text-primary text-balance lg:w-[500px]'>Hola! Soy Armando, un universitario de 21 años apasionado del desarrollo web autodidacta. Mi fascinación por la creación de sitios web comenzó como un hobby y ha evolucionado en una dedicación constante. El desarrollo web me ha permitido adquirir un amplio conocimiento en diferentes áreas, desde el diseño frontend hasta la implementación backend.</p>
+              </Fade>
             </article>
             <article className='lg:flex items-start gap-9 justify-between'>
-              <article className='w-40'>
-                <div className='w-20 h-[3px] bg-gradient-to-r from-blue-500 to-cyan-400 rounded-lg' />
-                <h1 className='text-4xl font-bold text-[#242424] dark:text-gray-300 tracking-widest pb-4'>Skills</h1>
-              </article>
-              <figure className='flex gap-9 flex-wrap lg:w-[500px]'>
-               {
-                skills.map(({id, img}) => (
-                  <div key={id} className='p-2 dark:bg-[#242424] rounded-xl'>
-                    <img className='h-6 w-6 hover:scale-150 duration-150 hover:rotate-3' src={img} key={id}/>
-                  </div>
-                ))
-               }
-              </figure>
+              <Fade direction='left' duration={400} delay={400}>
+                <article className='w-40'>
+                  <div className='w-20 h-[3px] bg-gradient-to-r from-blue-500 to-cyan-400 rounded-lg' />
+                  <h1 className='text-4xl font-bold text-[#242424] dark:text-gray-300 tracking-widest pb-4'>Skills</h1>
+                </article>
+              </Fade>
+              <Fade duration={400} direction='up' delay={400}>
+                <figure className='flex gap-9 flex-wrap lg:w-[500px]'>
+                 {
+                  skills.map(({id, img}) => (
+                    <div key={id} className='p-2 dark:bg-[#242424] rounded-xl'>
+                      <img className='h-6 w-6 hover:scale-150 duration-150 hover:rotate-3' src={img} key={id}/>
+                    </div>
+                  ))
+                 }
+                </figure>
+              </Fade>
             </article>
           </section>
         </section>
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,25 +1,30 @@
 import { FaGithub, FaLinkedin } from 'react-icons/fa'
 import { Container, Wrapper } from '../../components'
+import { Fade } from 'react-awesome-reveal'
 
 export default function Home() {
   return (
     <Wrapper>
       <Container>
-        <article className='flex flex-col gap-4'>
-          <h1 className='text-6xl font-bold tracking-wider leading-tight dark:text-gray-300'>
-            Hello <br /> im <span className='text-secondary'>Armando</span>
-          </h1>
-          <h2 className='text-3xl text-secondary font-semibold'>Front-End Web Developer</h2>
-          <p className='text-lg text-primary font-medium max-w-[700px]'>
-          ¡Hola! Soy Armando, un desarrollo web autodidacta. Mi fascinación por la creación de sitios web comenzó como un hobby y ha evolucionado en una dedicación constante. 
-          Dispuesto a explorar continuamente nuevas tecnologías para mejorar mis habilidades.
-          </p>
-          <ul className='flex gap-6'>
-            <li><a href='https://github.com/Chencho34' target='BLANK'><FaGithub className='dark:fill-gray-300' size={22}/></a></li>
-            <li><a href='https://www.linkedin.com/in/armando-cr/' target='BLANK'><FaLinkedin className='dark:fill-gray-300' size={22}/></a></li>
-          </ul>
-          <button className='bg-[#161616] dark:bg-gray-300 dark:text-[#161616] rounded-sm w-28 text-white font-semibold text-sm py-1'>Resume</button>
-        </article>
+        <Fade direction='left' duration={400} >
+          <article className='flex flex-col gap-4'>
+            <h1 className='text-6xl font-bold tracking-wider leading-tight dark:text-gray-300'>
+              Hello <br /> im <span className='text-secondary'>Armando</span>
+            </h1>
+            <h2 className='text-3xl text-secondary font-semibold'>Front-End Web Developer</h2>
+            <p className='text-lg text-primary font-medium max-w-[700px]'>
+            ¡Hola! Soy Armando, un desarrollo web autodidacta. Mi fascinación por la creación de sitios web comenzó como un hobby y ha evolucionado en una dedicación constante.
+            Dispuesto a explorar continuamente nuevas tecnologías para mejorar mis habilidades.
+            </p>
+            <ul className='flex gap-6'>
+              <Fade direction='left' duration={400}>
+                <li><a href='https://github.com/Chencho34' target='BLANK'><FaGithub className='dark:fill-gray-300' size={22}/></a></li>
+                <li><a href='https://www.linkedin.com/in/armando-cr/' target='BLANK'><FaLinkedin className='dark:fill-gray-300' size={22}/></a></li>
+              </Fade>
+            </ul>
+            <Fade direction='left' duration={400}><button className='bg-[#161616] dark:bg-gray-300 dark:text-[#161616] rounded-sm w-28 text-white font-semibold text-sm py-1'>Resume</button></Fade>
+          </article>
+        </Fade>
       </Container>
     </Wrapper>
   )
