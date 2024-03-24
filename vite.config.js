6619f7b5e997f453651ee61b020384fbe0691d81
Armando <armandocrescencio343@gmail.com>
2024-03-24 02:33:57 -0600

    FIX: wrapper component

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,4 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   base: 'https://Chencho34.github.io/MyPortfolio',
-  server: {
-    host: '192.168.1.69'
-  }
 })
