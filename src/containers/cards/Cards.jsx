6c64ad69061c9e43b7089e88aca40ec1f3885782
Armando <armandocrescencio343@gmail.com>
2024-05-19 15:54:26 -0600

    UPDATE A NEW PROJECT

--- src/containers/cards/Cards.jsx
+++ src/containers/cards/Cards.jsx
@@ -5,9 +5,9 @@ import { projects } from '../../constants'
 export default function Cards () {
   return (
     <>
-      {projects.map(({ id, title, image, description, tecnologies, github }) => (
+      {projects.map(({ id, title, image, description, tecnologies, github, demo }) => (
         <Fade key={id} duration={400}>
-          <Card key={id} image={image} title={title} description={description} tecnologies={tecnologies} github={github} /> 
+          <Card key={id} image={image} title={title} description={description} tecnologies={tecnologies} github={github} demo={demo} /> 
         </Fade>
       ))}
     </>
